930055d8c4209f3275f65c9611f424f9fbe02287
para <aayushtanwar9910@gmail.com>
2025-03-30 21:54:54 +0530

    Update

--- src/controllers/otpController.js
+++ src/controllers/otpController.js
@@ -50,15 +50,15 @@ const sendOtp = async (req, res) => {
       `http://sms.paragalaxy.com/smpp_api/sms?token=0e396ff2ce9d865fff42aefcd3b71def&To=${mobile_number}&Text=Your%20verification%20code%20is%20${otp}.%20Please%20enter%20OTP%20to%20confirm%20mobile%20number.%20PARAHIT%20REALTY%20PRIVATE%20LIMITED&tid=1607100000000341418`  
     );
 
-    res.json(response.data);
-
-
-
+    // res.json(response.data);
     // Respond with success message
-    res.status(200).json({ 
-      message: 'OTP sent successfully',
-      otp: otp
-     });
+       // Respond with success message
+       res.status(200).json({
+        message: 'OTP sent successfully',
+        otp: otp,  // Remove this in production for security reasons
+        smsResponse: response.data
+      });
+  
   } catch (error) {
     console.error('Error processing OTP request:', error); // Log detailed error for debugging
     res.status(500).json({ message: 'An error occurred while processing the OTP request.' });
