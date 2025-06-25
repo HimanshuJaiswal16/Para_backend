d6c38b61e7b23e30d551ba5c9159f95cf4584c62
AMD <aayushtanwar@gmail.com>
2025-06-26 00:27:55 +0530

    db changed

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,29 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'document_upload_db',
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'document_upload_db',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   logging: true
+// });
+
+// const promisePool = pool.promise();
+
+// module.exports = promisePool;
+
+const promisePool = mysql.createPool({
+host: 'database-1.clkwcssiqip4.ap-south-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'admin',
+ password: 'iq6jmXRklqR9eATkVuqp',
+  database: 'paraDB',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  logging: true
 });
 
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = { promisePool };
